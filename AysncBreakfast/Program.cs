aeaa0f0aa1842252f0e77912a1721d333cdb03b5
Sean Parker <sean.parker@x-labsystems.co.uk>
2021-09-06 11:51:14 +0100

    Added methods for synchronous program

--- AysncBreakfast/Program.cs
+++ AysncBreakfast/Program.cs
@@ -14,7 +14,60 @@ namespace AysncBreakfast
 
         private static Juice PourOJ()
         {
-            Console.WriteLine("Pouring OJ");
+            Console.WriteLine("Pouring Orange Juice");
+            return new Juice();
+        }
+
+        private static void ApllyJam(Toast toast) =>
+            Console.WriteLine("Putting Jam on Toast");
+
+        private static void ApplyButter(Toast toast) =>
+            Console.WriteLine("Putting Butter on la toast");
+
+        private static Toast ToastBread(int slices)
+        {
+            for (int slice = 0; slice < slices; slice++)
+            {
+                Console.WriteLine("Putting slice in toaster");
+            }
+            Console.WriteLine("Starting toasting....");
+            Task.Delay(3000).Wait();
+            Console.WriteLine("Remove le toast");
+            return new Toast();
+        }
+
+        private static Bacon FryBacon(int slices)
+        {
+            Console.WriteLine($"putting {slices} slices of bacon in the pan");
+            Console.WriteLine("cooking first side of bacon...");
+            Task.Delay(3000).Wait();
+            for (int slice = 0; slice < slices; slice++)
+            {
+                Console.WriteLine("flipping a slice of bacon");
+            }
+            Console.WriteLine("cooking the second side of bacon...");
+            Task.Delay(3000).Wait();
+            Console.WriteLine("Put bacon on plate");
+
+            return new Bacon();
+        }
+
+        private static Egg FryEggs(int howMany)
+        {
+            Console.WriteLine("Warming the egg pan...");
+            Task.Delay(3000).Wait();
+            Console.WriteLine($"cracking {howMany} eggs");
+            Console.WriteLine("cooking the eggs ...");
+            Task.Delay(3000).Wait();
+            Console.WriteLine("Put eggs on plate");
+
+            return new Egg();
+        }
+
+        private static Coffee PourCoffee()
+        {
+            Console.WriteLine("Pouring coffee");
+            return new Coffee();
         }
 
     }
